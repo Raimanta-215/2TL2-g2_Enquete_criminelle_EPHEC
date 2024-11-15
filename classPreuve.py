ff10f96c3edacee054149a5597559480e42156d7
Marry3005 <m.pierrard@students.ephec.be>
2024-11-15 10:52:38 +0100

    Update classPreuve.py

--- classPreuve.py
+++ classPreuve.py
@@ -1,10 +1,17 @@
 from datetime import datetime
 
+<<<<<<< Updated upstream
 
 class Preuve:
     def __init__(self, id_preuve, type_preuve, categorie, description, emplacement, image, date_decouverte):
         """
         Initialise une nouvelle instance de la classe Preuve.
+=======
+class Preuve:
+    def __init__(self, id_preuve, type_preuve, categorie, description, emplacement, image, date_decouverte):
+        """
+        initialise une nouvelle instance de la classe Preuve
+>>>>>>> Stashed changes
 
         :param id_preuve: int, Identifiant unique de la preuve.
         :param type_preuve: str, Type de la preuve.
@@ -14,6 +21,7 @@ class Preuve:
         :param image: str, Chemin ou URL de l'image de la preuve.
         :param date_decouverte: str, Date de découverte de la preuve au format 'DD-MM-YYYY'.
 
+<<<<<<< Updated upstream
         PRE: - 'id_preuve' est entier positif
              - 'date_decouverte' est chaine au format 'DD-MM-YYYY'
         POST: - preuve initialisée avec valeurs fournies
@@ -27,6 +35,21 @@ class Preuve:
             date_decouverte_str = datetime.strptime(date_decouverte, "%d-%m-%Y")
         except ValueError:
             raise ValueError("La date doit être au format 'DD-MM-YYYY'.")
+=======
+
+        PRE: - 'id_preuve' doit être entier positif
+             - 'date_decouverte' doit être une chaine "DD-MM-YYY"
+        POST: - nouvelle instance de preuve initialisée avec valeurs fournies
+        """
+
+        if not isinstance(id_preuve, int) or id_preuve < 0:
+            raise ValueError("Id doit être un entier positif")
+
+        try:
+            date_decouverte_str=datetime.strptime(date_decouverte,'%d-%m-%Y')
+        except ValueError:
+            raise ValueError("La date doit être au format DD-MM-YYY")
+>>>>>>> Stashed changes
 
         self.id_preuve = id_preuve
         self.type_preuve = type_preuve
@@ -35,8 +58,13 @@ class Preuve:
         self.emplacement = emplacement
         self.image = image
         self.date_decouverte = date_decouverte_str
+<<<<<<< Updated upstream
         self.analyses = []  # Liste pour stocker les analyses associées
         self.suspects = []  # Liste pour stocker les suspects associés
+=======
+        self.analyses = [] #liste stocker analyses
+        self.suspects = [] #liste stocker suspects
+>>>>>>> Stashed changes
 
     def ajouter_preuve(self, type_preuve, description, emplacement, image, date_decouverte):
         """
@@ -45,6 +73,7 @@ class Preuve:
         PRE: - arguments sont des chaine non vide et 'date_decouverte' doit être sous format "DD-MM-YYYY"
         POST: - preuve ajouter avec les valeurs fournies
         """
+<<<<<<< Updated upstream
         if not all(isinstance(arg, str) and arg for arg in [type_preuve, description, emplacement, image]):
             raise ValueError("type_preuve, description, emplacement, et image doivent être des chaînes non vides.")
 
@@ -52,13 +81,28 @@ class Preuve:
             date_obj = datetime.strptime(date_decouverte, "%d-%m-%Y")
         except ValueError:
             raise ValueError("La date doit être au format 'DD-MM-YYYY'.")
+=======
+
+        if not all(isinstance(arg, str) and arg for arg in [type_preuve, description, emplacement, image, date_decouverte]):
+            raise ValueError("type_preuve, description, emplacement, image, date_decouverte doivent être chaine non vide")
+
+        try:
+            date_str= datetime.strptime(date_decouverte,'%d-%m-%Y')
+        except ValueError:
+            raise ValueError("La date doit être un format DD-MM-YYYY")
+>>>>>>> Stashed changes
 
         self.type_preuve = type_preuve
         self.description = description
         self.emplacement = emplacement
         self.image = image
+<<<<<<< Updated upstream
         self.date_decouverte = date_obj
         print("Preuve ajoutée avec succès.")
+=======
+        self.date_decouverte = date_str
+        print("preuve bien ajoutée ")
+>>>>>>> Stashed changes
 
     def modifier_preuve(self, type_preuve=None, categorie=None, description=None, emplacement=None, image=None, date_decouverte=None):
         """
@@ -67,6 +111,10 @@ class Preuve:
         PRE: - arguments fournis sont des chaines et 'date_decouverte' est au format DD-MM-YYYY
         POST: - attributs de preuve mis à jour avec nouvelles valeurs fournies
         """
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
         if type_preuve is not None:
             self.type_preuve = type_preuve
         if categorie is not None:
@@ -79,12 +127,22 @@ class Preuve:
             self.image = image
         if date_decouverte is not None:
             try:
+<<<<<<< Updated upstream
                 self.date_decouverte = datetime.strptime(date_decouverte, "%d-%m-%Y")
             except ValueError:
                 raise ValueError("La date doit être au format 'DD-MM-YYYY'.")
         print("Preuve modifiée avec succès.")
 
     def ajouter_analyse(self, analyse):
+=======
+                self.date_decouverte = datetime.strptime(date_decouverte,'%d-%m-%Y')
+            except ValueError:
+                raise ValueError("La date doit être un format DD-MM-YYYY")
+
+        print("Preuve modifiée")
+
+    def ajouter_analyse(self, analyses):
+>>>>>>> Stashed changes
         """
         ajouter analyse à la liste
 
@@ -92,10 +150,18 @@ class Preuve:
         POST: -analyse ajouter a la liste des analyses
 
         """
+<<<<<<< Updated upstream
         if not isinstance(analyse, dict) or 'type' not in analyse or 'resultat' not in analyse:
             raise ValueError("L'analyse doit être un dictionnaire contenant les clés 'type' et 'resultat'.")
         self.analyses.append(analyse)
         print("Analyse ajoutée:", analyse)
+=======
+        if not isinstance(analyses, dict) or 'type' not in analyses or 'resultat' not in analyses:
+            raise ValueError("L'analyse doit être un dictionnaire content clé 'type' et 'resultat'")
+
+        self.analyses.append(analyses)
+        print ("Analyse ajouté")
+>>>>>>> Stashed changes
 
     def change_etat_analyse(self, index, nouvel_etat):
         """
@@ -105,6 +171,7 @@ class Preuve:
              - nouvel_etat est chaine non vide
         POST: - état de l'analyse à l'indice spécifié est modifié
         """
+<<<<<<< Updated upstream
         if not (0 <= index < len(self.analyses)):
             raise IndexError("Index d'analyse invalide.")
         if not isinstance(nouvel_etat, str) or not nouvel_etat:
@@ -112,6 +179,16 @@ class Preuve:
 
         self.analyses[index]['etat'] = nouvel_etat
         print(f"État de l'analyse {index} changé à {nouvel_etat}.")
+=======
+        if not(0 <= index < len(self.analyses)):
+            raise ValueError("Index analyse invalide")
+        if not isinstance(nouvel_etat, str) or not nouvel_etat:
+            raise ValueError("Nouvel etat doit être une chaine non vide")
+
+        self.analyses[index]['etat'] = nouvel_etat
+        print(f"Etat analyse {index} changé à {nouvel_etat}.")
+
+>>>>>>> Stashed changes
 
     def associer_suspect(self, suspect):
         """
@@ -121,6 +198,7 @@ class Preuve:
         POST: - suspect ajouté a la liste suspects
         """
         if not isinstance(suspect, str) or not suspect:
+<<<<<<< Updated upstream
             raise ValueError("Le suspect doit être une chaîne non vide.")
         self.suspects.append(suspect)
         print(f"Suspect associé: {suspect}")
@@ -131,6 +209,16 @@ class Preuve:
 
         Postconditions:
         - Les informations de la preuve sont affichées.
+=======
+            raise ValueError("Suspect doit être chaine non vide")
+        self.suspects.append(suspect)
+
+    def afficher_preuve(self):
+        """
+        afficher toute la preuve
+         PRE:
+         POST: -les info de la preuve sont afficher
+>>>>>>> Stashed changes
         """
         print(f"ID Preuve: {self.id_preuve}")
         print(f"Type de Preuve: {self.type_preuve}")
@@ -139,7 +227,15 @@ class Preuve:
         print(f"Emplacement: {self.emplacement}")
         print(f"Image: {self.image}")
         print(f"Date de Découverte: {self.date_decouverte.strftime('%d-%m-%Y')}")
+<<<<<<< Updated upstream
         print("Analyses:")
         for i, analyse in enumerate(self.analyses, 1):
             print(f"  - Analyse {i}: {analyse}")
         print("Suspects associés:", ", ".join(self.suspects))
+=======
+        print("Analyses détaillées;")
+        for analyse in self.analyses:
+            print(f"Type: {analyse.get('type')}, Resultat: {analyse.get('resultat')}")
+        print("Suspects associés:", ", ".join(self.suspects))
+
+>>>>>>> Stashed changes
